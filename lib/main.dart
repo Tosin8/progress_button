550fd9655a76c08ce2f2adf634bcb8e17dcfbb26
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-07-15 17:32:26 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:progress_state_button/iconed_button.dart';
 import 'package:progress_state_button/progress_button.dart';
-import 'dart:math'; 
-
+import 'dart:math';
 
 void main() {
   runApp(const MaterialApp(title: 'Progress Demo', home: MyApp()));
@@ -16,57 +15,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   ButtonState stateOnlyText = ButtonState.idle;
-    ButtonState stateTextWithIcon = ButtonState.idle;
+  ButtonState stateTextWithIcon = ButtonState.idle;
   @override
   Widget build(BuildContext context) {
-    
-
     return Scaffold(
         body: Center(
-      child: ProgressButton.icon(
-        iconedButtons: {
+      child: ProgressButton.icon(iconedButtons: {
         ButtonState.idle: IconedButton(
-          text: 'Send',
-          icon: const Icon(Icons.send, color: Colors.white),
-          color: Colors.deepPurple.shade500), 
-          ButtonState.loading: IconedButton(
-            text: 'Loading', 
-            color: Colors.deepPurple.shade700
-            ),
-          ButtonState.fail: IconedButton(
-            text: 'Failed', 
+            text: 'Send',
+            icon: const Icon(Icons.send, color: Colors.white),
+            color: Colors.deepPurple.shade500),
+        ButtonState.loading:
+            IconedButton(text: 'Loading', color: Colors.deepPurple.shade700),
+        ButtonState.fail: IconedButton(
+            text: 'Failed',
+            icon: const Icon(Icons.cancel, color: Colors.white),
+            color: Colors.red.shade300),
+        ButtonState.success: IconedButton(
+            text: 'Success',
             icon: const Icon(
-              Icons.cancel, color: Colors.white), 
-          color: Colors.red.shade300), 
-          ButtonState.success: const IconedButton(text: 'Success': icon: Icon(Icons.check_circle, color: Colors.white, ), 
-          color: Colors.green.shade400
-        ), 
-      },onPressed: onPressedIconWithText, state: stateTextWithIcon),
+              Icons.check_circle,
+              color: Colors.white,
+            ),
+            color: Colors.green.shade400),
+      }, onPressed: onPressedIconWithText, state: stateTextWithIcon),
     ));
   }
 }
 
-
-void onPressedIconWithText(){
+void onPressedIconWithText() {
   switch (stateTextWithIcon) {
-    case ButtonState.idle: stateTextWithIcon = ButtonState.loading;
-    Future.delayed(const Duration(seconds: 1), 
-    (){
-      setState((){
-        stateTextWithIcon = Random.secure().nextBool() ? ButtonState.success : ButtonState.fail;
-      }); 
-    }); 
-     break;
+    case ButtonState.idle:
+      stateTextWithIcon = ButtonState.loading;
+      Future.delayed(const Duration(seconds: 1), () {
+        setState(() {
+          stateTextWithIcon = Random.secure().nextBool()
+              ? ButtonState.success
+              : ButtonState.fail;
+        });
+      });
+      break;
 
-    case ButtonState.loading: break;
-    case ButtonState.success: stateTextWithIcon = ButtonState.idle; break;
-    case ButtonState.fail: stateTextWithIcon = ButtonState.idle; break;
+    case ButtonState.loading:
+      break;
+    case ButtonState.success:
+      stateTextWithIcon = ButtonState.idle;
+      break;
+    case ButtonState.fail:
+      stateTextWithIcon = ButtonState.idle;
+      break;
   }
   setState(
     () {
       stateTextWithIcon = stateTextWithIcon;
     },
-    );
+  );
 }
